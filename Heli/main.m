7dce922c5eb3fdf7ea379aaaf59f12ada1496223
Petar <mac@petars-air.speedport.ip>
2014-09-03 20:57:27 +0200

    Initial Commit

---
+++ Heli/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Heli
+//
+//  Created by Petar on 9/3/14.
+//  Copyright (c) 2014 Petar Popovski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ HeliTests/HeliTests.m
@@ -0,0 +1,34 @@
+//
+//  HeliTests.m
+//  HeliTests
+//
+//  Created by Petar on 9/3/14.
+//  Copyright (c) 2014 Petar Popovski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HeliTests : XCTestCase
+
+@end
+
+@implementation HeliTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
